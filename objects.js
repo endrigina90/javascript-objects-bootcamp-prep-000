4f70e2e49bff4bbe14af02e6c74129b2dec0eb12
Endri Gina <endrigina90@gmail.com>
2017-11-08 21:15:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artist, song){
   playlist[artist] = song
   return playlist
 }
+function removeFromPlaylist(artist, song){
+  delete playlist.artist
+  return playlist
+}
