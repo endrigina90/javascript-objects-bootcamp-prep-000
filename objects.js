74bd5e19ae43202dfced5ae691e13bc403f67258
Endri Gina <endrigina90@gmail.com>
2017-11-08 21:12:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { Rockstar: "Nickleback", ThreeDoorsDown: "Its Not My Time", Marron5: "She will be loved" };
 
 function updatePlaylist(playlist, key, value){
-  playlist.key = value
+  playlist[key] = value
   return playlist
 }
